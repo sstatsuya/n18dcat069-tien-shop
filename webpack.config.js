2cbb546b2c50e98787ced02b8867e4d39380d18e
luongminhtien <tienluongminh@thegioididong.com>
2024-06-26 09:03:48 +0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = {
-  "crypto": require.resolve("crypto-browserify")
+  crypto: require.resolve("crypto-browserify"),
+  rules: {
+    test: /\.m?js/,
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+  },
 };
