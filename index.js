d960d98d6bcb576fe5875573c3881502cd27dd3a
luongminhtien <tienluongminh@thegioididong.com>
2024-06-26 08:44:41 +0700

    fix

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-import './src/index'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./src/index.css";
+import App from "./src/App";
+import reportWebVitals from "./src/reportWebVitals";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import { toast } from "react-toastify";
+import { Provider } from "react-redux";
+import store from "./src/reducer/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+toast.configure();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
